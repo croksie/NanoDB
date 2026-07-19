7428e1d16df63db991e3f50bc6be23964f49f1a2
Guillaume <guillaume.servant@etu.iut-tlse3.fr>
2026-07-19 11:18:27 +0200

    feat : add serialization and deserialization dunctions in Tuple struct

--- Tuple.cpp
+++ Tuple.cpp
@@ -1,1 +1,25 @@
 #include "Tuple.h"
+#include "DatabaseException.h"
+
+
+Tuple Tuple::serialize(const std::vector<std::unique_ptr<DataValue>>& values, const std::vector<Column>& columns) {
+    std::vector<uint8_t> result;
+    for (size_t i = 0; i < columns.size(); i++) {
+        Column c = columns.at(i);
+        if (c.getDataType()->getTypeName() != values.at(i)->type) {
+            throw DatabaseException("Types de donnee incompatibles lors de la serialisation");
+        }
+        std::vector<uint8_t> tmp = c.getDataType()->serialize(values.at(i)->getValue());
+        result.insert(result.end(), tmp.begin(), tmp.end());
+    }
+    return Tuple(result);
+}
+std::vector<std::shared_ptr<DataValue>> Tuple::deserialize(const std::vector<Column>& columns) const {
+    std::vector<std::shared_ptr<DataValue>> result;
+    size_t offset = 0;
+    for (size_t i = 0; i < columns.size(); i++) {
+        Column c = columns.at(i);
+        result.push_back(c.getDataType()->deserialize(this->data, offset));
+    }
+    return result;
+}
--- Tuple.h
+++ Tuple.h
@@ -1,10 +1,19 @@
 #pragma once
 #include <vector>
 #include <cstdint>
+
+#include "Column.h"
+#include "DataValue.h"
+
+
+
 struct Tuple
 {
 	std::vector<uint8_t> data;
 
 	Tuple(std::vector<uint8_t> data) : data(data) {}
+	
+	static Tuple serialize(const std::vector<std::unique_ptr<DataValue>>& values, const std::vector<Column>& columns);
+	std::vector<std::shared_ptr<DataValue>> deserialize(const std::vector<Column>& columns) const;
 };
 
